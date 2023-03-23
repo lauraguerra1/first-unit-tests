20d8e0208dd65218c7adfd62915259e23acfe15b
Laura Guerra <l.garciaguerra1@gmail.com>
2023-03-23 14:00:48 -0400

    Pass all tests

--- functions.js
+++ functions.js
@@ -1,9 +1,31 @@
 // functions.js
-function addTwoNumbers() {
+function addTwoNumbers(num1, num2) {
+  var sum = num1 + num2;
+  return sum;
+}
+function sayHello(name) {
+  var greeting = `Hi there ${name}!`;
+  var meanGreeting = `No more testing ${name}!`;
+  if (name === "Will") {
+    return meanGreeting;
+  } else {
+    return greeting;
+  }
+}
+function buildCar(color, type) {
+  if (color || type) {
+    var car = {
+      color: color,
+      type: type,
+    };
+    return car;
+  } else {
+    return {};
+  }
 }
 
 module.exports = {
-    addTwoNumbers,
-    // sayHello,
-    // buildCar
-}
+  addTwoNumbers,
+  sayHello,
+  buildCar,
+};
